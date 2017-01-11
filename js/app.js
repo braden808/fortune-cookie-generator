6f380e09ddfdabb19e7aa73aa6532e7401e14cad
Braden Amorozo <braden808@gmail.com>
2017-01-11 09:20:25 -1000

    update

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
-function generateFortuneCookie(){
-    var fortunesList = ["You are amazing", "you suck", "you're cool", "you will be rich", "you will be successful", "you will fail"];
-    var node = document.createElement("LI");
-    var textnode = document.createTextNode(fortunesList);
-    node.appendChild(textnode);
-    document.getElementById("myList").appendChild(node);
-     for(int i = 0; i < fortunesList.length; i++) { 
-		fortunesList[i] = i +1; 
+var = ["you suck", "you suck more", "you suck the most", "you sucky suck suck", "you're zlatan"];
+
+ function generateFortune() {
+ 	console.log(hello)
 }
