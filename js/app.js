4a53d3838f296177b73e8a48f5561e42bfce939e
Braden Amorozo <braden808@gmail.com>
2017-01-13 09:21:47 -1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
-var = ["you suck", "you suck more", "you suck the most", "you sucky suck suck", "you're zlatan"];
+var names = ["you suck", "you suck more", "you suck the most", "you sucky suck suck", "you're zlatan"];
 
- function generateFortune() {
- 	console.log(hello)
+
+
+
+function makeFortune() {
+	
+	var localarray = names.slice();
+	var node = document.createElement("LI");
+	for (var i = 4; i >= 0; i--) {
+ 	randomFortune = localarray.splice(Math.floor(Math.random() * (i + 1)), 1);
+	}
+	console.log(randomFortune)
+    randomFortune.appendChild(node);
 }
