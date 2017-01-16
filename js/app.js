7b4869ee1ef6eed819d77f85e455f6b7a307f6d5
Braden Amorozo <braden808@gmail.com>
2017-01-15 16:09:46 -1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
 var names = ["you suck", "you suck more", "you suck the most", "you sucky suck suck", "you're zlatan"];
 
-
-
-
 function makeFortune() {
-	
 	var localarray = names.slice();
-	var node = document.createElement("LI");
-	for (var i = 4; i >= 0; i--) {
- 	randomFortune = localarray.splice(Math.floor(Math.random() * (i + 1)), 1);
-	}
-	console.log(randomFortune)
-    randomFortune.appendChild(node);
+    var node = document.createElement("LI");
+	var textnode = document.createTextNode(names[]);
+    for (var i = 4; i >= 0; i--) {
+      randomFortune = (Math.floor(Math.random() * (i + 1)), 1);;
+    }
+    node.appendChild(textnode);
+    document.getElementById("fortune-cookie-text").appendChild(node);
 }
